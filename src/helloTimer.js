c94291f7b6182aedaa064dbd468b41a2a6f0dc3b
DiganmeGiovanni <giovanni.fi05@gmail.com>
2013-12-26 23:32:22 -0600

    Agregando ejemplos de setTimeout

---
+++ src/helloTimer.js
@@ -0,0 +1,5 @@
+setTimeout(function(){
+    console.log("World");
+}, 3000);
+
+console.log("hello");
---
+++ src/helloTimer2.js
@@ -0,0 +1,5 @@
+setInterval(function(){
+    console.log("World");
+}, 3000);
+
+console.log("hello");
