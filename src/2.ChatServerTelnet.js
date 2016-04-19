e78d4aff3538a4be2e5b2f736213960d8c1b7a2c
Giovanni Aguirre <giovanni.fi05@gmail.com>
2016-04-19 11:21:01 -0500

    :tada: Chat server through telnet

---
+++ src/2.ChatServerTelnet.js
@@ -0,0 +1,52 @@
+
+/**
+ * A basic chat server
+ *
+ * How to use?
+ * 1. Open a terminal and type: 'telnet <server-ip> <server-port>'
+ *    Sample: ~> telnet localhost 2121
+ * 2. Write message and type enter to send to each chat connected user
+ *
+ * Type command from as many client machines as you want and each
+ * machine will be a chat user.
+ */
+
+var net = require('net')
+var sockets = []
+
+var server = net.Server(function (socket) {
+
+  sockets.push(socket)
+
+  //
+  // when user sends data through its socket
+  // forward message to each connected user.
+  socket.on('data', function(data) {
+    for (var i=0; i< sockets.length; i++) {
+      if (socket != sockets[i]) {
+        sockets[i].write(data)
+      }
+    }
+  })
+
+  //
+  // when user disconnects, remove its socket from
+  // array and inform to remaining users.
+  socket.on('end', function() {
+    var socketIndex = sockets.indexOf(socket)
+    sockets.splice(i, 1)
+
+    for (var i=0; i< sockets.length; i++) {
+      var msg = 'A user leave the char'
+      msg += ' | Connected users: ' + sockets.length
+      sockets[i].write(msg)
+    }
+  })
+
+})
+
+//
+// Start chat server on port 2121
+server.listen(2121)
+console.log('Chat server listening on port 2121')
+console.log(' * Connect with "telnet localhost 2121"')
